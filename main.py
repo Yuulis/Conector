c15b49c66f707372c3871a482362cd4baf7069c0
Yuulis <yuulis2005@gmail.com>
2023-05-04 18:13:31 +0900

    画面遷移を実装

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+
+
+# 画面遷移
+def change_window(window):
+    window.tkraise()
+    
+    # textラベル更新フェーズに移行
+    root.after(10, update_text)
 
 
 # textラベル更新
-def updateText():
+def update_text():
     global text
     global count
     global after_id
@@ -13,7 +22,7 @@ def updateText():
         text="CONNECTING" + "." * (count % 4)
     )
     
-    # 250ms * 10まで"CONNECTING..."を表示
+    # 250ms*10まで"CONNECTING..."を表示
     if count > 10:
         text.config(
             text="SUCCESS! 524kHz",
@@ -21,10 +30,10 @@ def updateText():
         )
         root.after_cancel(after_id)
     else:    
-        after_id = root.after(250, updateText)
+        after_id = root.after(250, update_text)
 
 
-# ウィンドウ画面設定
+# rootメインウィンドウ設定
 root = tk.Tk()
 root.update_idletasks()
 width = 720
@@ -34,24 +43,56 @@ sh = root.winfo_screenheight()
 root.geometry(str(width)+"x"+str(height)+"+"+str(int(sw/2-width/2))+"+"+str(int(sh/2-height/2)))
 root.title("Connector")
 
-frame = tk.Frame(root, width=500, height=480, padx=50, pady=200)
-frame.pack()
+# rootメインウィンドウのグリッドを1x1にする
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
+# フレーム配置
+frame_input = ttk.Frame(root)
+frame_input.grid(row=0, column=0, sticky=tk.NSEW, pady=20)
+frame_connection = ttk.Frame(root)
+frame_connection.grid(row=0, column=0, sticky=tk.NSEW, pady=20)
 
-# ラベルウィジェット作成
-text = tk.Label(
-    frame,
-    text="CONNECTING",
-    font=("Consolas", 30, "bold"),
+"""
+ウィジェットリスト
+# frame_input
+    - pos_input : 位置情報入力用
+    - button : 画面遷移用
+
+# frame_connection
+    - text : 接続表示用
+"""
+pos_input = ttk.Entry(
+    frame_input,
+    width=30,
+    font=("BIZ UDゴシック", 20),
+    justify=tk.CENTER,
+)
+pos_input.pack()
+
+style = ttk.Style().configure("pos_input.TButton", font=("Consolas", 20, "bold"))
+button = ttk.Button(
+    frame_input,
+    command=lambda: change_window(frame_connection),
+    width=10,
+    padding=[10],
+    style="pos_input.TButton",
+    text="Connect",
+)
+button.pack()
+
+text = ttk.Label(
+    frame_connection,
     width=13,
-    anchor=tk.W
+    anchor=tk.W,
+    font=("Consolas", 30, "bold"),
+    text="CONNECTING",
 )
 text.pack(anchor="center", expand=1)
 
 # 更新フェーズカウンター
 count = 0
 
-# textラベル更新フェーズに移行
-root.after(10, updateText)
+frame_input.tkraise()
 
 root.mainloop()
